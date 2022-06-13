5c558261a23acbb8b84e14e0360d5864b342e10c
sabine-xuaitsep <sabinepestiaux@gmail.com>
2022-06-13 05:14:46 +0200

    apolloClient converted to module

--- client/src/main.js
+++ client/src/main.js
@@ -1,24 +1,9 @@
 import { createApp, provide, h } from 'vue'
 import { createPinia } from "pinia"
 import router from "./routes/index"
-import App from './App.vue'
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
 import { DefaultApolloClient } from '@vue/apollo-composable'
-
-// HTTP connection to the API
-const httpLink = createHttpLink({
-  // You should use an absolute URL here
-  uri: 'http://localhost:4000',
-})
-
-// Cache implementation
-const cache = new InMemoryCache()
-
-// Create the apollo client
-const apolloClient = new ApolloClient({
-  link: httpLink,
-  cache,
-})
+import { apolloClient } from './apolloClient'
+import App from './App.vue'
 
 const app = createApp({
   setup () {
